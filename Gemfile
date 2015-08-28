175d4f2aa6a2bd02baa493a738639ff00198c5a6
jcsfbass <jcsfbass@gmail.com>
2015-08-27 22:32:55 -0300

    Testando funcionalidade de retornar json

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.2.2'
 
 gem 'sinatra', '~> 1.4.0'
 gem 'thin', '~> 1.6.0'
+gem 'json', '~> 1.8.0'
--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'json'
 
 get '/'  do
-	'Hello, world!'
+	content_type :json
+	{
+		array: [
+			object: {
+				string: '',
+				integer: 1,
+				boolen: true
+			}
+		]
+	}.to_json
 end
