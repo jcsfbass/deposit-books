9ab5e9be5838ad0f6c56b16a12b4f2e5b0a5a3be
jcsfbass <jcsfbass@gmail.com>
2015-09-18 21:24:17 -0300

    Melhorando lógica para buscar por id do livro

--- app/server.rb
+++ app/server.rb
@@ -27,15 +27,14 @@ get '/livros' do
 end
 
 get '/livros/:id' do |id|
-	@books.first.to_json
+	@books.each do |book|
+		return book.to_json if book[:id].to_s === id
+	end
+	halt 404
 end
 
 post '/livros' do
 	halt 201, @books.first.to_json
-end
-
-delete '/livros' do
-	@books.first.to_json
 end
 
 delete '/livros/:id' do |id|
