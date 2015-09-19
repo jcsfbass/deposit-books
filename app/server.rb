046e4de2831ce3a058570e9cd183e808305b808b
jcsfbass <jcsfbass@gmail.com>
2015-09-18 21:06:46 -0300

    Adicionando campos aos objetos

--- app/server.rb
+++ app/server.rb
@@ -6,14 +6,18 @@ before do
 
 	@books = [
 		{
+			id: 1,
 			description: 'description 1',
 			author: 'author 1',
-			edition: 'edition 1'
+			edition: 'edition 1',
+			quantity: 5
 		},
 		{
+			id: 2,
 			description: 'description 2',
 			author: 'author 2',
-			edition: 'edition 2'
+			edition: 'edition 2',
+			quantity: 6
 		}
 	]
 end
