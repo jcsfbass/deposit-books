7b000a36f9b0a12f32ab946c046f7a1505f2787f
jcsfbass <jcsfbass@gmail.com>
2015-09-29 21:17:01 -0300

    Adicionando testes para BookRepository.find

--- tests/unit/book_repository_spec.rb
+++ tests/unit/book_repository_spec.rb
@@ -1,9 +1,31 @@
+require 'securerandom'
+
 describe BookRepository do
 	describe '.all' do
 		subject(:books) { BookRepository.all }
 
 		it 'should return a array with books' do
 			books.each { |book| expect(book).to be_an_instance_of(Book) }
+		end
+	end
+
+	describe '.find' do
+		subject(:book) { BookRepository.find(id) }
+
+		context 'when find existent book' do
+		  let(:id) { BookRepository.all.first.id }
+
+		  it 'should return a book correctly' do
+		    expect(book.id).to eq(id)
+		  end
+		end
+
+		context 'when find nonexistent book' do
+		  let(:id) { SecureRandom.uuid }
+
+		  it 'should return nil' do
+		    expect(book).to be_nil
+		  end
 		end
 	end
 end
