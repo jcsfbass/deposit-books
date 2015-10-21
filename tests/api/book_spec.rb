4bb5f9f53fef96889503001a45463822e8dce081
jcsfbass <jcsfbass@gmail.com>
2015-10-21 19:08:37 -0300

    Adicionando teste para GET /livros/:id

--- tests/api/book_spec.rb
+++ tests/api/book_spec.rb
@@ -1,4 +1,5 @@
 require 'uuid'
+require 'securerandom'
 require 'rack/test'
 
 describe 'Books' do
@@ -8,20 +9,46 @@ describe 'Books' do
 		Sinatra::Application
 	end
 
-  let(:body) { JSON.parse(last_response.body) }
+  before { get '/livros' }
+  let(:books) { JSON.parse(last_response.body) }
 
   describe 'GET /livros' do
-  	before(:all) { get '/livros' }
-
     it 'should return status 200' do
       expect(last_response).to be_ok
     end
 
     it 'should return an array of books' do
-      body.each do |element|
-      	expect(element).to be_book
-      end
+      books.each { |book| expect(book).to be_book }
     end
+  end
+
+  describe 'GET /livros/:id' do
+  	before { get "/livros/#{id}" }
+  	let(:response) { JSON.parse(last_response.body) }
+
+  	context 'when find existent book' do
+  		let(:id) { books.first['id'] }
+
+  		it 'should return status 200' do
+	      expect(last_response).to be_ok
+	    end
+
+  	  it 'should return a book' do
+  	    expect(response).to be_book
+  	  end
+  	end
+
+  	context 'when find nonexistent book' do
+  	  let(:id) { SecureRandom.uuid }
+
+  	  it 'should return status 404' do
+	      expect(last_response).to be_not_found
+	    end
+
+	    it 'should return a error message' do
+  	    expect(response['message']).to eq('Book not found')
+  	  end
+  	end
   end
 end
 
